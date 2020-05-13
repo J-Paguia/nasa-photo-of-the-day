0f4ded55f4f7711922a2687b74c0abedcc6a97a2
unknown <futurewave27@yahoo.com>
2020-05-13 15:51:00 -0700

    Partial Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import Container from './component/Card/Container'
 
 function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        {/* Read through the instructions in the README.md file to build your NASA
+        app! Have fun <span role="img" aria-label='go!'>🚀</span>! */}
       </p>
+      <Container />
     </div>
   );
 }
